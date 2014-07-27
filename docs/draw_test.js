986094556d6204ad2606bbc8996e33aac5d904e0
nham <hamann.nick@gmail.com>
2014-07-27 17:05:34 -0400

    Don't use global variables in draw_test

--- docs/draw_test.js
+++ docs/draw_test.js
@@ -1,27 +1,33 @@
 var canvas = new fabric.Canvas('c');
 
-var circs = [];
 
-var grid_start = { x: 50, y: 50 };
-var coord_to_pos = function(i, j) {
+// start is a dictionary { x: <number>, y: <number> }
+// i, j are numbers
+var coord_to_pos = function(start, i, j) {
     var h_spacing = 60;
     var v_spacing = 60;
-    return { x: grid_start.x + h_spacing*i,
-             y: grid_start.y + v_spacing*j
+    return { x: start.x + h_spacing*i,
+             y: start.y + v_spacing*j
     };
 };
 
-var rows = 6;
-var cols = 4;
-for (var i = 0; i < rows; i++) {
-    for (var j = 0; j < cols; j++) {
-        var pos = coord_to_pos(i, j);
-        circs.push(new fabric.Circle({
-            radius: 3, fill: 'green', left: pos.x, top: pos.y
-        }));
-    }
-}
+var grid_dots = function(rows, cols) {
+    var circs = [];
+    var grid_start = { x: 50, y: 50 };
 
-for (var i = 0; i < circs.length; i++) {
-    canvas.add(circs[i]);
-}
+    for (var i = 0; i < rows; i++) {
+        for (var j = 0; j < cols; j++) {
+            var pos = coord_to_pos(grid_start, i, j);
+            circs.push(new fabric.Circle({
+                radius: 3, fill: 'green', left: pos.x, top: pos.y
+            }));
+        }
+    }
+
+    for (var i = 0; i < circs.length; i++) {
+        canvas.add(circs[i]);
+    }
+};
+
+grid_dots(6,4);
+
