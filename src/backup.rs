426da9d69206191e043f48858db1d5ac1550e814
nham <hamann.nick@gmail.com>
2014-07-29 23:14:26 -0400

    Change copy_dir_ignore to take a parameter specifying whether it should create the root dir. Add a new function for initializing the .igno directory

--- src/backup.rs
+++ src/backup.rs
@@ -1,15 +1,18 @@
+use std::io;
 use std::io::IoResult;
 use std::io::fs::{readdir, copy, lstat, mkdir};
 use std::collections::HashSet;
 
 
-fn copy_dir_ignore(from: &Path, to: &Path, ignore: &HashSet<Path>) -> IoResult<()> {
+fn copy_dir_ignore(from: &Path, to: &Path, create: bool, ignore: &HashSet<Path>) -> IoResult<()> {
     if !from.is_dir() {
         fail!("source isn't a directory");
     }
 
-    try!(mkdir(to, try!(lstat(from)).perm));
-    println!("created {}", to.display());
+    if create {
+        try!(mkdir(to, try!(lstat(from)).perm));
+        println!("created {}", to.display());
+    }
 
     for p in try!(readdir(from)).iter() {
         if ignore.contains(p) {
@@ -28,7 +31,7 @@ fn copy_dir_ignore(from: &Path, to: &Path, ignore: &HashSet<Path>) -> IoResult<(
             let pnew = Path::new(vec);
 
             if p.is_dir() {
-                try!(copy_dir_ignore(p, &pnew, ignore));
+                try!(copy_dir_ignore(p, &pnew, true, ignore));
             } else {
                 try!(copy(p, &pnew));
                 println!("copied {} to {}", p.display(), pnew.display());
@@ -38,11 +41,30 @@ fn copy_dir_ignore(from: &Path, to: &Path, ignore: &HashSet<Path>) -> IoResult<(
     Ok(())
 }
 
+// If the .igno directory already exists, return false. If it was successfully
+// created, return true. Otherwise an error is returned
+fn igno_init() -> IoResult<bool> {
+    let igno = Path::new(".igno");
+    if igno.is_dir() {
+        Ok(false)
+    } else {
+        try!(mkdir(&igno, io::UserDir));
+        Ok(true)
+    }
+}
+
 
 fn main() {
     let curr = Path::new(".");
     let ig_path = Path::new(".igno");
+
+    match igno_init() {
+        Err(e) => println!("{}", e),
+        Ok(false) => {},
+        Ok(true) => println!("Created .igno"),
+    }
+
     let mut ignore = HashSet::new();
     ignore.insert(ig_path.clone());
-    println!("{}", copy_dir_ignore(&curr, &ig_path, &ignore));
+    println!("{}", copy_dir_ignore(&curr, &ig_path, false, &ignore));
 }
