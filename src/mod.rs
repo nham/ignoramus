b1dd91ff41f63e4276322ef5cb0dfbb36a6704cd
nham <hamann.nick@gmail.com>
2014-08-14 13:03:22 -0400

    Move argument parsing into its own function. Make CheckoutLatest its own variant of Command and do the  error checking for that inside exec instead of parsing

--- src/mod.rs
+++ src/mod.rs
@@ -28,16 +28,20 @@ fn igno_init() -> IoResult<bool> {
     }
 }
 
+fn igno_is_init() -> bool {
+    Path::new(".igno").is_dir()
+}
+
 fn get_next_snapshot_num() -> IoResult<uint> {
-    match get_highest_numdir(&Path::new(".igno")) {
+    match get_highest_snapshot_num() {
         Err(e) => Err(e),
         Ok(None) => Ok(0u),
         Ok(Some(n)) => Ok(n+1),
     }
 }
 
-fn igno_is_init() -> bool {
-    Path::new(".igno").is_dir()
+fn get_highest_snapshot_num() -> IoResult<Option<uint>> {
+    get_highest_numdir(&Path::new(".igno"))
 }
 
 fn update_head(n: uint) -> IoResult<()> {
@@ -83,6 +87,7 @@ enum Command {
     Init,
     Snapshot,
     Checkout(uint),
+    CheckoutLatest,
 }
 
 fn exec(cmd: Command) {
@@ -102,50 +107,58 @@ fn exec(cmd: Command) {
             match checkout(n) {
                 Err(e) => println!("Error: {}", e),
                 Ok(_) => println!("Snapshot checked out"),
-
             }
         },
+        CheckoutLatest => {
+            match get_highest_snapshot_num() {
+                Err(e) => println!("Error: {}", e),
+                Ok(None) => println!("There aren't any snapshots to check out."),
+                Ok(Some(n)) => exec(Checkout(n)),
+            }
+        },
+    }
+}
+
+fn parse_args(args: &[String]) -> Result<Command, &'static str> {
+    let cnr = "Command not recognized.";
+    let checkout_arg = "Argument to 'checkout' must either be an integer or 'latest'";
+    match args.len() {
+        2 =>
+            if args[0].equiv(&"checkout") {
+                if args[1].equiv(&"latest") {
+                    Ok(CheckoutLatest)
+                } else {
+                    // try to parse argument as uint
+                    let d: Option<uint> = from_str(args[1].as_slice());
+                    match d {
+                        None => Err(checkout_arg),
+                        Some(n) => Ok(Checkout(n)),
+                    }
+                }
+            } else {
+                Err(cnr)
+            },
+        1 =>
+            if args[0].equiv(&"init") {
+                Ok(Init)
+            } else {
+                Err(cnr)
+            },
+        0 =>
+            if igno_is_init() {
+                Ok(Snapshot)
+            } else {
+                Err("This is not an ignoramus repository")
+            },
+        _ => Err(cnr),
     }
 }
 
 fn main() {
     let args = os::args();
 
-    let mut cmd = None;
-    if args.len() == 3 {
-        if args[1].equiv(&"checkout") {
-            if args[2].equiv(&"latest") {
-                match get_highest_numdir(&Path::new(".igno")) {
-                    Err(e) => fail!("{}", e),
-                    Ok(None) => fail!("No snapshots have been made yet."),
-                    Ok(Some(n)) => cmd = Some(Checkout(n)),
-                }
-            } else {
-                let d: Option<uint> = from_str(args[2].as_slice());
-                match d {
-                    None => {},
-                    Some(n) => cmd = Some(Checkout(n)),
-                }
-            }
-
-            if cmd.is_none() {
-                fail!("Argument to 'checkout' must either be an integer or 'latest'");
-            }
-        }
-    } else if args.len() == 2 && args[1].equiv(&"init") {
-        cmd = Some(Init);
-    } else if args.len() == 1 {
-        if !igno_is_init() {
-            fail!("This is not an ignoramus repository");
-        } else {
-            cmd = Some(Snapshot);
-        }
+    match parse_args(args.slice_from(1)) {
+        Err(e) => fail!("{}", e),
+        Ok(c) => exec(c),
     }
-
-    let cmd = match cmd {
-        None => fail!("Command not recognized"),
-        Some(c) => c,
-    };
-
-    exec(cmd);
 }
