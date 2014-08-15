ad28de5ed09ff9a5d3c509fa17ae0c284bc6a966
nham <hamann.nick@gmail.com>
2014-08-15 16:38:36 -0400

    Generalize the make_callback thingy to animate_sequence. Make draw_grid_lines return an object of down lines and right lines

--- docs/diffdraw.js
+++ docs/diffdraw.js
@@ -41,24 +41,59 @@ var draw_diag_line = function(s, start, coord1, coord2) {
 // are 2D arrays of lines, where down[i][j] is the downward line *starting*
 // at node (i, j) (and ditto for 'right'
 var draw_grid_lines = function(s, start, rows, cols) {
-    var lines = [];
+    var lines = {down: [], right: []};
     for (var i = 0; i < rows; i++) {
-        lines.push([]);
+        lines.down.push([]);
+        lines.right.push([]);
         for (var j = 0; j < cols; j++) {
             var pos = coord_to_pos(start, i, j);
 
             // draw the downward lines
             if (i !== rows - 1) {
                 var posd = coord_to_pos(start, i+1, j);
-                lines[i].push( draw_line(s, pos.x, pos.y, posd.x, posd.y) );
+                lines.down[i].push( draw_line(s, pos.x, pos.y, posd.x, posd.y) );
             }
 
             // draw the rightward line
             if (j !== cols - 1) {
                 var posr = coord_to_pos(start, i, j+1);
-                lines[i].push( draw_line(s, pos.x, pos.y, posr.x, posr.y) );
+                lines.right[i].push( draw_line(s, pos.x, pos.y, posr.x, posr.y) );
             }
         }
     }
     return lines;
+};
+
+// this whole dumb thing is for recursively generated nested callbacks
+// in order to sequence Snap.svg animations. pass in a sequence of Snap.svg
+// elements.
+//
+// also the animation logic is currently hard-coded. to make this fully generic,
+// need to pass in an animation function of some sort. but i only need this!
+var animation_sequencer = function(seq) {
+    var delay = 600;
+    var fill_stroke = {fill: "#ff6d1f", stroke: "#ff2d1f"};
+
+    var make_non_terminal_callback = function(n) {
+        return function() {
+            seq[n].animate(fill_stroke, delay, null,
+                                  make_callback(n+1));
+        };
+    };
+
+    var make_terminal_callback = function(n) {
+        return function() {
+            seq[n].animate(fill_stroke, delay);
+        };
+    };
+
+    var make_callback = function(n) {
+        if(n === seq.length - 1) {
+            return make_terminal_callback(n);
+        } else {
+            return make_non_terminal_callback(n);
+        }
+    };
+
+    return make_callback(0);
 };
